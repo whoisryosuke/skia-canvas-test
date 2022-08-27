08059bc8f90d8453f6347cdda0b5f474fbc62aef
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-08-26 18:13:21 -0700

    :zap: input detection via mouse + keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Window } from "skia-canvas";
 import store from "./store";
 
-let win = new Window(300, 300);
+let win = new Window(300, 300,{background:'rgba(16, 16, 16, 0.35)'});
 win.title = "Canvas Window";
 win.on("draw", (e) => {
   let ctx = e.target.canvas.getContext("2d");
-  console.log("test", e.target);
+  // console.log("test", e.target);
   ctx.lineWidth = 25 + 25 * Math.cos(e.frame / 10);
   ctx.beginPath();
   ctx.arc(150, 150, 50, 0, 2 * Math.PI);
@@ -16,3 +16,36 @@ win.on("draw", (e) => {
   ctx.stroke();
   ctx.fill();
 });
+
+win.on('mousemove', ({button, x, y, target}) => {
+  let ctx = target.canvas.getContext("2d");
+  // const { canvas, ctx } = win;
+  if (button == 0){ // a left click
+    ctx.fillStyle = `rgb(${Math.floor(255 * Math.random())},50,0)`
+    ctx.beginPath()
+    ctx.arc(x, y, 10 + 30 * Math.random(), 0, 2 * Math.PI)
+    ctx.fill()
+    console.log('left click!')
+  }
+
+  if (button == 1){ // a left click
+    console.log('middle click!')
+  }
+
+  if (button == 2){ // a left click
+    console.log('right click!')
+  }
+
+  win.cursor = button === 0 ? 'none' : 'crosshair'
+})
+
+win.on('keydown', ({key, target}) => {
+  let ctx = target.canvas.getContext("2d");
+  // const { canvas, ctx } = win;
+  if (key == 'Escape'){
+    // ctx.clearRect(0, 0, target.canvas.width, target.canvas.height)
+
+    // Close window
+    target.close()
+  }
+})
