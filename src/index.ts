1501f00854bf455653991e7a24533cf38082bf22
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-08 12:06:21 -0700

    :hammer: abstraction and cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
 import { CanvasRenderingContext2D, Window } from "skia-canvas";
 import store from "./store";
 
-class SkiaElement {
+abstract class SkiaComponent {
+
+  /**
+   * This runs after element's render.
+   * Lets user define "side-effect" logic when component finishes rendering
+   */
+  componentDidMount() {}
+  
+  /**
+   * This runs after element's render.
+   * Lets user define "side-effect" logic when component finishes rendering
+   */
+  componentDidUpdate() {}
+  
+  /**
+   * This runs when element is queued for destruction next tick. 
+   * Lets user define logic when component leaves scene.
+   */
+  componentWillUnmount() {}
+
+  /**
+   * The render lifecyle. Draw to context here.
+   */
+  render(ctx: CanvasRenderingContext2D) {}
+}
+
+class SkiaElement extends SkiaComponent {
+  /**
+   * Has element been created and rendered at least once?
+   */
+  mounted = false;
+  /**
+   * Is the element visible?
+   */
   visible = true;
+  /**
+   * Is the element queued for destruction?
+   */
   destroyFlag = false;
+
+  afterRender() {
+    // Mounting logic
+    if(!this.mounted) {
+      this.mounted = true;
+      this.componentDidMount();
+    }
+
+    this.componentDidUpdate();
+  }
 
   /**
    * Toggle the visibility of this element
@@ -18,9 +64,15 @@ class SkiaElement {
    * This means this class will be removed from scene graph next frame tick
    */
   destroy() {
+    // Call the unmount method 
+    this.componentWillUnmount();
     this.destroyFlag = true;
   }
 }
+
+const COLORS = [
+  'orange', 'yellow', 'green', 'skyblue', 'purple', 'teal', 'magenta'
+]
 
 class Button extends SkiaElement { 
   x = 0;
@@ -34,8 +86,14 @@ class Button extends SkiaElement {
   }
 
   render(ctx: CanvasRenderingContext2D) {
+    
     ctx.beginPath()
-    ctx.arc(this.x, this.y, 10 + 30 * Math.random(), 0, 2 * Math.PI)
+    // const colorKey = Math.round(Math.random() * COLORS.length -1);
+    const colorKey = this.x % COLORS.length - 1;
+    ctx.fillStyle = COLORS[colorKey]
+    // ctx.arc(this.x, this.y, 10 + 30 * Math.random(), 0, 2 * Math.PI)
+    const random = this.x % 3;
+    ctx.arc(this.x, this.y, 10 + 30 * random, 0, 2 * Math.PI)
     ctx.fill()
 
     this.lifecycle += 1;
@@ -53,7 +111,7 @@ const sceneGraph = {
   children: [new Button(0,0)],
 }
 
-let win = new Window(500, 500,{background:'rgba(16, 16, 16, 0.35)'});
+let win = new Window(800, 800,{background:'rgba(16, 16, 16, 0.35)'});
 win.title = "Canvas Window";
 win.on("draw", (e) => {
   let ctx = e.target.canvas.getContext("2d");
@@ -63,7 +121,11 @@ win.on("draw", (e) => {
 
   // Render all elements
   //@ts-ignore
-  sceneGraph.children.map((child) => child.render(ctx))
+  sceneGraph.children.forEach((child) => {
+    if(!child.visible) return;
+    child.render(ctx);
+    child.afterRender();
+  })
 
   // We can also use Zustand store to manage state directly
   // const { bears } = store.getState()
@@ -78,11 +140,9 @@ win.on("draw", (e) => {
 win.on('mousemove', ({button, x, y, target, ctrlKey, altKey, shiftKey, metaKey, pageX, pageY, ...rest}) => {
   let ctx = target.canvas.getContext("2d");
   // const { canvas, ctx } = win;
-  if (!shiftKey && button == 0){ // a left click
-    ctx.fillStyle = `rgb(${Math.floor(255 * Math.random())},50,0)`
-    ctx.beginPath()
-    ctx.arc(x, y, 10 + 30 * Math.random(), 0, 2 * Math.PI)
-    ctx.fill()
+
+  // Left click
+  if (!shiftKey && button == 0){ 
     console.log('left click!', pageX, pageY)
 
     // Zustand example
@@ -100,11 +160,13 @@ win.on('mousemove', ({button, x, y, target, ctrlKey, altKey, shiftKey, metaKey, 
 
   }
 
-  if (button == 1){ // a left click
+  // Middle click
+  if (button == 1){ 
     console.log('middle click!')
   }
 
-  if (button == 2){ // a left click
+  // Right click
+  if (button == 2){ 
     console.log('right click!')
   }
 
